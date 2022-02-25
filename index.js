278cb27326e377b1c71ddcdef7a6c486f9cd9dd6
Kamenev Maksim <MegaRoks@gmail.com>
2022-02-25 13:12:15 +0300

    Fix index file

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./graphql/schema');
-const rootValue = require('./graphql/root');
-const app = express();
 
 const todoRouters = require('./routes/todo');
 
+const app = express();
+
 app.use(express.json());
 app.use('/api/todo', todoRouters);
-app.use('/graphql',graphqlHTTP({
-  schema,
-  rootValue,
-  graphiql: true,
-}));
 
 async function start() {
-  const PORT = process.env.PORT || 3001;
+  const PORT = 3001;
 
   app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
   });
 }
-
 start();
